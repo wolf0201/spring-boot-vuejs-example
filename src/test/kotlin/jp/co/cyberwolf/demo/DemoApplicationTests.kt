c1fe7036582268b0ff42202c6b5dee738d4ffa61
Toshimi <toshimikusunoki@gmail.com>
2020-07-04 11:17:36 +0900

    環境構築

---
+++ src/test/kotlin/jp/co/cyberwolf/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.co.cyberwolf.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
